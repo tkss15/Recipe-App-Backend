4134cbc715036e5a2288f33882e3ae3f0439d9ca
Eldad Shneor <eldadshneor@gmail.com>
2023-07-02 18:29:39 +0300

    added another front end

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'https://recipe-app-tkss15.vercel.app',
     'https://www.recipe-app-tkss15.vercel.app',
+    'https://recipesforeveryone.vercel.app',
     'https://www.eshneor.com',
     'http://localhost:3000'
 ]
