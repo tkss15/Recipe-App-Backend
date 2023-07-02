9845fe80de45ae0881cd0a1c3d94fec881f35c8c
Eldad Shneor <38609871+tkss15@users.noreply.github.com>
2023-07-02 12:20:16 +0300

    Update corsOrigin.js

--- config/corsOrigin.js
+++ config/corsOrigin.js
@@ -2,7 +2,7 @@ const allowedOrigins = require('./allowedOrigins');
 
 const corsSettings = {
     origin: (origin, callback) => {
-        if(allowedOrigins.indexOf(origin) !== -1)
+        if(allowedOrigins.indexOf(origin) !== -1 || !origin)
         {
             callback(null,true);
         }
